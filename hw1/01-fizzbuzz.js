2ed8728bc57a06a44e5be3c8869efbc4336164dc
matt <madler@pdx.edu>
2022-09-28 17:09:09 -0700

    hw1

--- hw1/01-fizzbuzz.js
+++ hw1/01-fizzbuzz.js
@@ -1,5 +1,15 @@
 const fizzbuzz = () => {
-  // Add your code here
+  for (i = 0; i< 100; ++i){
+    if(i%3==0){
+      console.log("fizz");
+    }
+    else if (i%5==0){
+      console.log('buzz')
+    }
+    else{
+     console.log(i); 
+    }
+  }
 };
 
 fizzbuzz();
