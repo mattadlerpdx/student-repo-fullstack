dd211e212102620d80c231ac53a3d550311cf210
matt <madler@pdx.edu>
2022-09-28 17:09:09 -0700

    hw1

--- hw1/01-fizzbuzz.js
+++ hw1/01-fizzbuzz.js
@@ -10,7 +10,17 @@ Use console.log() to write the proper output to the command line.
 **/
 
 const fizzbuzz = () => {
-  // Add your code here
+  for (i = 0; i< 100; ++i){
+    if(i%3==0){
+      console.log("fizz");
+    }
+    else if (i%5==0){
+      console.log('buzz')
+    }
+    else{
+     console.log(i); 
+    }
+  }
 };
 
 fizzbuzz();
