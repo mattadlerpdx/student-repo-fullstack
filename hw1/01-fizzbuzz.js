2384143d1bdb9138c60ab9540cf884c5c04c5e09
matt <madler@pdx.edu>
2022-09-30 15:43:21 -0700

    fizzbuzz prints

--- hw1/01-fizzbuzz.js
+++ hw1/01-fizzbuzz.js
@@ -11,10 +11,13 @@ Use console.log() to write the proper output to the command line.
 
 const fizzbuzz = () => {
   for (i = 0; i< 100; ++i){
-    if(i%3==0){
+     if (i % 3 === 0 && i % 5 === 0) {
+       console.log("fizzbuzz");
+     }
+    else if(i%3 === 0){
       console.log("fizz");
     }
-    else if (i%5==0){
+    else if (i%5 === 0){
       console.log('buzz')
     }
     else{
