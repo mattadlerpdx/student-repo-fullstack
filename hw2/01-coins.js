ad06f18c4cd68e7008932c0763e7b87262ed38d3
matt <madler@pdx.edu>
2022-10-17 13:53:00 -0700

    01 coins works except returning undefined in output

--- hw2/01-coins.js
+++ hw2/01-coins.js
@@ -2,36 +2,93 @@
 
 const calculateChange = (input) => {
   //modulo input for each element in array until change === 0
-  var coins = 0;
-  var i = 0;
+  let coins = 0;
+
   //use outputArray to output correct amounts
-  var outputArray = [0, 0, 0, 0, 0];
+  let outputArray = [0, 0, 0, 0, 0];
   const coinArray = [1, 0.25, 0.1, 0.05, 0.01];
   change = Number(input);
+
   if (change > 10) {
     console.log("Error: the number is too large");
   } else {
-    for (var i = 0; i < 5; i += 1) {
+    let i = 0;
+    while (i < 5) {
       console.log("index:", i);
-      if (change < coinArray[i]) {
-        //if change is less than current index value go to nex index
-        i += 1;
-      } else {
+      if (change > coinArray[i]) {
         //get denomination
         coins = Math.floor(change / coinArray[i]);
-        //console.log(coins);
         //store denominations
         outputArray[i] = coins;
-        console.log(outputArray[i]);
         //get next largest denomination
         change = change % coinArray[i];
+      } else {
+        i += 1;
       }
     }
+  }
+  consoleOutput(outputArray);
+};
+
+const consoleOutput = (coinArray) => {
+  dollar = dollarOutput(coinArray[0]);
+  quarter = quarterOutput(coinArray[1]);
+  dime = dimeOutput(coinArray[2]);
+  nickel = nickelOutput(coinArray[3]);
+  penny = pennyOutput(coinArray[4]);
+
+  console.log(`${dollar} ${quarter} ${dime} ${nickel} ${penny}`);
+};
+const dollarOutput = (input) => {
+  if (input === 0) {
+    return null;
+  } else if (input === 1) {
+    return "1 dollar";
+  } else {
+    return `${input} dollars`;
+  }
+};
+const quarterOutput = (input) => {
+  if (input === 0) {
+    return null;
+  } else if (input === 1) {
+    return "quarter";
+  } else {
+    return `${input} quarters`;
+  }
+};
+
+const dimeOutput = (input) => {
+  if (input === 0) {
+    return null;
+  } else if (input === 1) {
+    return "1 dime";
+  } else {
+    return `${input} dimes`;
+  }
+};
+
+const nickelOutput = (input) => {
+  if (input === 0) {
+    return;
+  } else if (input === 1) {
+    return "1 nickel";
+  } else {
+    return `${input} nickels`;
+  }
+};
+const pennyOutput = (input) => {
+  if (input === 0) {
+    return null;
+  } else if (input === 1) {
+    return "1 penny";
+  } else {
+    return `${input} pennies`;
   }
 };
 
 // Sample Test Cases
-//console.log(calculateChange(4.62));
+console.log(calculateChange(4.62));
 // $4.62 ==> 4 dollars, 2 quarters, 1 dime, 2 pennies
 console.log(calculateChange(9.74));
 // $9.74 ==> 9 dollars, 2 quarters, 2 dimes, 4 pennies
